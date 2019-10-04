4271d9edb8553a1dffcd1fbd278a7533a067b99c
DarthAmun <37881139+DarthAmun@users.noreply.github.com>
2019-10-04 11:54:08 +0200

    fixed landing page or home

--- src/components/App.js
+++ src/components/App.js
@@ -22,27 +22,108 @@ import TopNav from './TopNav';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <MemoryRouter>
-          <LeftNav />
-          <div id="content">
-            <TopNav />
-            <Switch>
-              <Route path="/spell-overview" component={SpellOverview} />
-              <Route path="/item-overview" component={ItemOverview} />
-              <Route path="/monster-overview" component={MonsterOverview} />
-              <Route path="/char-overview" component={CharOverview} />
-              <Route path="/char/:id" component={CharView} />
-              <Route path="/add-view" component={AddView} />
-              <Route path="/add-spell" component={AddSpell} />
-              <Route path="/add-item" component={AddItem} />
-              <Route path="/options" component={Options} />
-              <Route path="/" component={Home} />
-            </Switch>
-          </div>
-        </MemoryRouter>
-        <div id="credits">by DarthAmun</div>
-      </div>
+      <MemoryRouter>
+        <Switch>
+          <Route path="/spell-overview" render={() => {
+            return <div className="App">
+              <LeftNav />
+              <div id="content">
+                <TopNav />
+                <SpellOverview />
+              </div>
+              <div id="credits">by DarthAmun</div>
+            </div>
+          }} />
+          <Route path="/item-overview" render={() => {
+            return <div className="App">
+              <LeftNav />
+              <div id="content">
+                <TopNav />
+                <ItemOverview />
+              </div>
+              <div id="credits">by DarthAmun</div>
+            </div>
+          }} />
+          <Route path="/monster-overview" render={() => {
+            return <div className="App">
+              <LeftNav />
+              <div id="content">
+                <TopNav />
+                <MonsterOverview />
+              </div>
+              <div id="credits">by DarthAmun</div>
+            </div>
+          }} />
+          <Route path="/char-overview" render={() => {
+            return <div className="App">
+              <LeftNav />
+              <div id="content">
+                <TopNav />
+                <CharOverview />
+              </div>
+              <div id="credits">by DarthAmun</div>
+            </div>
+          }} />
+          <Route path="/char/:id" render={() => {
+            return <div className="App">
+              <LeftNav />
+              <div id="content">
+                <TopNav />
+                <CharView />
+              </div>
+              <div id="credits">by DarthAmun</div>
+            </div>
+          }} />
+          <Route path="/add-view" render={() => {
+            return <div className="App">
+              <LeftNav />
+              <div id="content">
+                <TopNav />
+                <AddView />
+              </div>
+              <div id="credits">by DarthAmun</div>
+            </div>
+          }} />
+          <Route path="/add-spell" render={() => {
+            return <div className="App">
+              <LeftNav />
+              <div id="content">
+                <TopNav />
+                <AddSpell />
+              </div>
+              <div id="credits">by DarthAmun</div>
+            </div>
+          }} />
+          <Route path="/add-item" render={() => {
+            return <div className="App">
+              <LeftNav />
+              <div id="content">
+                <TopNav />
+                <AddItem />
+              </div>
+              <div id="credits">by DarthAmun</div>
+            </div>
+          }} />
+          <Route path="/options" render={() => {
+            return <div className="App">
+              <LeftNav />
+              <div id="content">
+                <TopNav />
+                <Option />
+              </div>
+              <div id="credits">by DarthAmun</div>
+            </div>
+          }} />
+          <Route path="/" render={() => {
+            return <div className="App">
+              <div id="content">
+                <Home />
+              </div>
+              <div id="credits">by DarthAmun</div>
+            </div>
+          }} />
+        </Switch>
+      </MemoryRouter>
     );
   }
 }
