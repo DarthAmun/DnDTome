09cea0052763b6b720df1a44bea857d3272a1e99
DarthAmun <37881139+DarthAmun@users.noreply.github.com>
2019-10-13 12:48:44 +0200

    converted the searchBars to hooks

--- src/components/item/ItemSearchBar.js
+++ src/components/item/ItemSearchBar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import '../../assets/css/SearchBar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUndo } from '@fortawesome/free-solid-svg-icons';
@@ -6,95 +6,37 @@ import { faUndo } from '@fortawesome/free-solid-svg-icons';
 const electron = window.require('electron');
 const ipcRenderer = electron.ipcRenderer;
 
-class ItemSearchBar extends Component {
-    state = {
-        query: {
-            name: "",
-            description: "",
-            rarity: "",
-            type: "",
-            source: ""
-        }
-    }
+export default function ItemSearchBar() {
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const [rarity, setRarity] = useState("");
+    const [type, setType] = useState("");
+    const [source, setSource] = useState("");
 
-    changeName = (event) => {
-        this.setState({
-            ...this.state,
-            query: {
-                ...this.state.query,
-                name: event.target.value
-            }
-        });
-    }
-    changeRarity = (event) => {
-        this.setState({
-            ...this.state,
-            query: {
-                ...this.state.query,
-                rarity: event.target.value
-            }
-        });
-    }
-    changeType = (event) => {
-        this.setState({
-            ...this.state,
-            query: {
-                ...this.state.query,
-                type: event.target.value
-            }
-        });
-    }
-    changeDescription = (event) => {
-        this.setState({
-            ...this.state,
-            query: {
-                ...this.state.query,
-                description: event.target.value
-            }
-        });
-    }
-    changeSource = (event) => {
-        this.setState({
-            ...this.state,
-            query: {
-                ...this.state.query,
-                source: event.target.value
-            }
-        });
-    }
-
-    sendQuery = (e) => {
+    function sendQuery(e) {
         if (e.key === 'Enter') {
-            ipcRenderer.send('sendItemSearchQuery', { query: this.state.query });
+            ipcRenderer.send('sendItemSearchQuery', { query: { name, description, rarity, type, source } });
         }
     }
 
-    resetSearch = (e) => {
-        this.setState({
-            ...this.state,
-            query: {
-                name: "",
-                description: "",
-                rarity: "",
-                type: "",
-                source: ""
-            }
-        });
+    function resetSearch(e) {
+        setType("");
+        setSource("");
+        setName("");
+        setRarity("");
+        setDescription("");
         ipcRenderer.send('sendItemSearchQuery', { query: {} });
     }
 
-    render() {
-        return (
-            <div id="searchBar">
-                <input type="text" style={{width: "180px"}} placeholder="Name" value={this.state.query.name} onChange={this.changeName} onKeyDown={this.sendQuery}></input>
-                <input type="text" style={{width: "180px"}} placeholder="Rarity" value={this.state.query.rarity} onChange={this.changeRarity} onKeyDown={this.sendQuery}></input>
-                <input type="text" style={{width: "180px"}} placeholder="Type" value={this.state.query.type} onChange={this.changeType} onKeyDown={this.sendQuery}></input>
-                <input type="text" style={{width: "180px"}} placeholder="Source" value={this.state.query.source} onChange={this.changeSource} onKeyDown={this.sendQuery}></input>
-                <input type="text" style={{width: "180px"}} placeholder="Description" value={this.state.query.description} onChange={this.changeDescription} onKeyDown={this.sendQuery}></input>
-                <button onClick={this.resetSearch}><FontAwesomeIcon icon={faUndo} /> Reset</button>
-            </div>
-        )
-    }
+    return (
+        <div id="searchBar">
+            <input type="text" style={{ width: "180px" }} placeholder="Name" value={name} onChange={e => setName(e.target.value)} onKeyDown={sendQuery}></input>
+            <input type="text" style={{ width: "180px" }} placeholder="Rarity" value={rarity} onChange={e => setRarity(e.target.value)} onKeyDown={sendQuery}></input>
+            <input type="text" style={{ width: "180px" }} placeholder="Type" value={type} onChange={e => setType(e.target.value)} onKeyDown={sendQuery}></input>
+            <input type="text" style={{ width: "180px" }} placeholder="Source" value={source} onChange={e => setSource(e.target.value)} onKeyDown={sendQuery}></input>
+            <input type="text" style={{ width: "180px" }} placeholder="Description" value={description} onChange={e => setDescription(e.target.value)} onKeyDown={sendQuery}></input>
+            <button onClick={resetSearch}><FontAwesomeIcon icon={faUndo} /> Reset</button>
+        </div>
+    )
 }
 
-export default ItemSearchBar;
--- src/components/monster/MonsterSearchBar.js
+++ src/components/monster/MonsterSearchBar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import '../../assets/css/SearchBar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUndo } from '@fortawesome/free-solid-svg-icons';
@@ -6,155 +6,52 @@ import { faUndo } from '@fortawesome/free-solid-svg-icons';
 const electron = window.require('electron');
 const ipcRenderer = electron.ipcRenderer;
 
-class MonsterSearchBar extends Component {
-    state = {
-        query: {
-            name: "",
-            type: "",
-            subtype: "",
-            cr: "",
-            alignment: "",
-            speed: "",
-            damage: "",
-            senses: "",
-            ability: "",
-            action: ""
-        }
-    }
+export default function MonsterSearchBar() {
+    const [name, setName] = useState("");
+    const [type, setType] = useState("");
+    const [subtype, setSubtype] = useState("");
+    const [cr, setCr] = useState("");
+    const [alignment, setAlignment] = useState("");
+    const [speed, setSpeed] = useState("");
+    const [damage, setDamage] = useState("");
+    const [senses, setSenses] = useState("");
+    const [ability, setAbility] = useState("");
+    const [action, setAction] = useState("");
 
-    changeName = (event) => {
-        this.setState({
-            ...this.state,
-            query: {
-                ...this.state.query,
-                name: event.target.value
-            }
-        });
-    }
-    changeType = (event) => {
-        this.setState({
-            ...this.state,
-            query: {
-                ...this.state.query,
-                type: event.target.value
-            }
-        });
-    }
-    changeSubtype = (event) => {
-        this.setState({
-            ...this.state,
-            query: {
-                ...this.state.query,
-                subtype: event.target.value
-            }
-        });
-    }
-    changeCR = (event) => {
-        this.setState({
-            ...this.state,
-            query: {
-                ...this.state.query,
-                cr: event.target.value
-            }
-        });
-    }
-    changeAlignment = (event) => {
-        this.setState({
-            ...this.state,
-            query: {
-                ...this.state.query,
-                alignment: event.target.value
-            }
-        });
-    }
-    changeSpeed = (event) => {
-        this.setState({
-            ...this.state,
-            query: {
-                ...this.state.query,
-                speed: event.target.value
-            }
-        });
-    }
-    changeDamage = (event) => {
-        this.setState({
-            ...this.state,
-            query: {
-                ...this.state.query,
-                damage: event.target.value
-            }
-        });
-    }
-    changeSenses = (event) => {
-        this.setState({
-            ...this.state,
-            query: {
-                ...this.state.query,
-                senses: event.target.value
-            }
-        });
-    }
-    changeAbility = (event) => {
-        this.setState({
-            ...this.state,
-            query: {
-                ...this.state.query,
-                ability: event.target.value
-            }
-        });
-    }
-    changeAction = (event) => {
-        this.setState({
-            ...this.state,
-            query: {
-                ...this.state.query,
-                action: event.target.value
-            }
-        });
-    }
-
-    sendQuery = (e) => {
+    function sendQuery(e) {
         if (e.key === 'Enter') {
-            ipcRenderer.send('sendMonsterSearchQuery', { query: this.state.query });
+            ipcRenderer.send('sendMonsterSearchQuery', { query: { name, type, subtype, cr, alignment, speed, damage, senses, ability, action } });
         }
     }
 
-    resetSearch = (e) => {
-        this.setState({
-            ...this.state,
-            query: {
-                name: "",
-                type: "",
-                subtype: "",
-                cr: "", 
-                alignment: "",
-                speed: "",
-                damage: "",
-                senses: "",
-                ability: "",
-                action: ""
-            }
-        });
+    function resetSearch(e) {
+        setName("");
+        setSenses("");
+        setSpeed("");
+        setType("");
+        setSubtype("");
+        setCr("");
+        setAbility("");
+        setDamage("");
+        setAlignment("");
+        setAction("");
         ipcRenderer.send('sendMonsterSearchQuery', { query: {} });
     }
 
-    render() {
-        return (
-            <div id="searchBar">
-                <input type="text" style={{width: "180px"}} placeholder="Name" value={this.state.query.name} onChange={this.changeName} onKeyDown={this.sendQuery}></input>
-                <input type="text" style={{width: "80px"}} placeholder="Type" value={this.state.query.type} onChange={this.changeType} onKeyDown={this.sendQuery}></input>
-                <input type="text" style={{width: "80px"}} placeholder="Subtype" value={this.state.query.subtype} onChange={this.changeSubtype} onKeyDown={this.sendQuery}></input>
-                <input type="text" style={{width: "20px"}} placeholder="Cr" value={this.state.query.cr} onChange={this.changeCR} onKeyDown={this.sendQuery}></input>
-                <input type="text" style={{width: "80px"}} placeholder="Alignment" value={this.state.query.alignment} onChange={this.changeAlignment} onKeyDown={this.sendQuery}></input>
-                <input type="text" style={{width: "80px"}} placeholder="Speed" value={this.state.query.speed} onChange={this.changeSpeed} onKeyDown={this.sendQuery}></input>
-                <input type="text" style={{width: "80px"}} placeholder="Senses" value={this.state.query.senses} onChange={this.changeSenses} onKeyDown={this.sendQuery}></input>
-                <input type="text" style={{width: "80px"}} placeholder="Damage" value={this.state.query.damage} onChange={this.changeDamage} onKeyDown={this.sendQuery}></input>
-                <input type="text" style={{width: "180px"}} placeholder="Ability" value={this.state.query.ability} onChange={this.changeAbility} onKeyDown={this.sendQuery}></input>
-                <input type="text" style={{width: "180px"}} placeholder="Action" value={this.state.query.action} onChange={this.changeAction} onKeyDown={this.sendQuery}></input>
-                <button onClick={this.resetSearch}><FontAwesomeIcon icon={faUndo} /> Reset</button>
-            </div>
-        )
-    }
-}
+    return (
+        <div id="searchBar">
+            <input type="text" style={{ width: "180px" }} placeholder="Name" value={name} onChange={e => setName(e.target.value)} onKeyDown={sendQuery}></input>
+            <input type="text" style={{ width: "80px" }} placeholder="Type" value={type} onChange={e => setType(e.target.value)} onKeyDown={sendQuery}></input>
+            <input type="text" style={{ width: "80px" }} placeholder="Subtype" value={subtype} onChange={e => setSubtype(e.target.value)} onKeyDown={sendQuery}></input>
+            <input type="text" style={{ width: "20px" }} placeholder="Cr" value={cr} onChange={e => setCr(e.target.value)} onKeyDown={sendQuery}></input>
+            <input type="text" style={{ width: "80px" }} placeholder="Alignment" value={alignment} onChange={e => setAlignment(e.target.value)} onKeyDown={sendQuery}></input>
+            <input type="text" style={{ width: "80px" }} placeholder="Speed" value={speed} onChange={e => setSpeed(e.target.value)} onKeyDown={sendQuery}></input>
+            <input type="text" style={{ width: "80px" }} placeholder="Senses" value={senses} onChange={e => setSenses(e.target.value)} onKeyDown={sendQuery}></input>
+            <input type="text" style={{ width: "80px" }} placeholder="Damage" value={damage} onChange={e => setDamage(e.target.value)} onKeyDown={sendQuery}></input>
+            <input type="text" style={{ width: "180px" }} placeholder="Ability" value={ability} onChange={e => setAbility(e.target.value)} onKeyDown={sendQuery}></input>
+            <input type="text" style={{ width: "180px" }} placeholder="Action" value={action} onChange={e => setAction(e.target.value)} onKeyDown={sendQuery}></input>
+            <button onClick={resetSearch}><FontAwesomeIcon icon={faUndo} /> Reset</button>
+        </div>
+    )
 
-export default MonsterSearchBar;
+}
