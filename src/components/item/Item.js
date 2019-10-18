96910d7ef6a637bef631e89fe7cea2e11eb538e0
DarthAmun <37881139+DarthAmun@users.noreply.github.com>
2019-10-18 22:05:57 +0200

    added default images

--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -1,32 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../../assets/css/item/Item.css';
+import icon from '../../assets/img/dice_icon_grey.png';
 
-class Item extends Component {
+export default function Item(props) {
 
-    getRarityClass = () => {
-        let rarityClass = this.props.item.item_rarity;
+    const getRarityClass = () => {
+        let rarityClass = props.item.item_rarity;
         rarityClass = rarityClass.replace("A*", "").trim();
         rarityClass = rarityClass.replace(/\s/g, "");
         return rarityClass;
     }
 
-    render() {
-        const style = {
-            backgroundImage: `url(${this.props.item.item_pic})`,
-            backgroundPosition: 'center',
-            backgroundSize: 'contain',
-            backgroundRepeat: 'no-repeat'
-        };
-
-        return (
-            <div className="item" style={{ animationDelay: `${this.props.delay * 50}ms` }} onClick={this.props.onClick}>
-                <div className={`itemIcon ${this.getRarityClass()}`} style={style}></div>
-                <div className="itemComp">{this.props.item.item_name}</div>
-                <div className="itemComp smallItemAttr">{this.props.item.item_type}</div>
-                <div className="itemComp smallItemAttr">{this.props.item.item_rarity}</div>
-            </div>
-        )
+    const getPicture = () => {
+        if (props.item.item_pic === "") {
+            return icon;
+        }
+        return props.item.item_pic;
     }
-}
 
-export default Item;
+    const style = {
+        backgroundImage: `url(${getPicture()})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'contain',
+        backgroundRepeat: 'no-repeat'
+    };
+
+    return (
+        <div className="item" style={{ animationDelay: `${props.delay * 50}ms` }} onClick={props.onClick}>
+            <div className={`itemIcon ${getRarityClass()}`} style={style}></div>
+            <div className="itemComp">{props.item.item_name}</div>
+            <div className="itemComp smallItemAttr">{props.item.item_type}</div>
+            <div className="itemComp smallItemAttr">{props.item.item_rarity}</div>
+        </div>
+    )
+}
--- src/components/monster/Monster.js
+++ src/components/monster/Monster.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../../assets/css/monster/Monster.css';
+import icon from '../../assets/img/dice_icon_grey.png';
 
-class Monster extends Component {
-    formatType = (value) => {
+export default function Monster(props) {
+    const formatType = (value) => {
         return value.charAt(0).toUpperCase() + value.slice(1);
     }
 
-    formatSubType = (value) => {
+    const formatSubType = (value) => {
         if (value == "") {
             return "";
         } else {
@@ -14,30 +15,34 @@ class Monster extends Component {
         }
     }
 
-    render() {
-        const style = {
-            backgroundImage: `url(${this.props.monster.monster_pic})`,
-            backgroundPosition: 'center',
-            backgroundSize: 'contain',
-            backgroundRepeat: 'no-repeat'
-        };
-
-        return (
-            <div className="monster" style={{ animationDelay: `${this.props.delay * 50}ms` }} onClick={this.props.onClick}>
-                <div className="monsterAttr monsterType">
-                    <b>{this.formatType(this.props.monster.monster_type)}</b>
-                    {this.formatSubType(this.props.monster.monster_subtype)}
-                </div>
-                <div className="monsterAttr monsterLevel">{this.props.monster.monster_cr}</div>
-                <div className="monsterIcon" style={style}></div>
-
-                <div className="monsterAttr monsterName"><b>{this.props.monster.monster_name}</b></div>
-
-                <div className="smallMonsterAttr"><b>Alignment: </b>{this.props.monster.monster_alignment}</div>
-                <div className="smallMonsterAttr"><b>Size: </b>{this.props.monster.monster_size}</div>
-            </div>
-        )
+    const getPicture = () => {
+        if (props.monster.monster_pic === "") {
+            return icon;
+        }
+        return props.monster.monster_pic;
     }
-}
 
-export default Monster;
+    const style = {
+        backgroundImage: `url(${getPicture()})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'contain',
+        backgroundRepeat: 'no-repeat'
+    };
+
+    return (
+        <div className="monster" style={{ animationDelay: `${props.delay * 50}ms` }} onClick={props.onClick}>
+            <div className="monsterAttr monsterType">
+                <b>{formatType(props.monster.monster_type)}</b>
+                {formatSubType(props.monster.monster_subtype)}
+            </div>
+            <div className="monsterAttr monsterLevel">{props.monster.monster_cr}</div>
+            <div className="monsterIcon" style={style}></div>
+
+            <div className="monsterAttr monsterName"><b>{props.monster.monster_name}</b></div>
+
+            <div className="smallMonsterAttr"><b>Alignment: </b>{props.monster.monster_alignment}</div>
+            <div className="smallMonsterAttr"><b>Size: </b>{props.monster.monster_size}</div>
+        </div>
+    )
+
+}
