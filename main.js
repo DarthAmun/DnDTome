72f5dae5679ea439c37e62c261faa66f1277adf1
DarthAmun <37881139+DarthAmun@users.noreply.github.com>
2019-10-23 19:56:03 +0200

    added and styled skill profs

--- main.js
+++ main.js
@@ -530,10 +530,13 @@ const saveMonster = (monster) => {
 const saveChar = (char) => {
   let data = [char.name, char.player, char.prof, char.exp, char.pic, char.classes, char.race, char.background, char.ac, char.hp, char.currentHp, char.hitDice,
   char.init, char.speed, char.str, char.dex, char.con, char.int, char.wis, char.cha, char.actions, char.bonusActions, char.reactions, char.features, char.classFeatures, char.racialFeatures,
-  char.profsLangs, char.senses, char.passivPerception, char.passivInsight, char.passivInvestigation, char.notesOne, char.notesTwo, char.notesThree, char.acrobatics,
-  char.animalHandling, char.arcana, char.athletics, char.deception, char.history, char.insight, char.intimidation,
+  char.profsLangs, char.senses, char.passivPerception, char.passivInsight, char.passivInvestigation, char.notesOne, char.notesTwo, char.notesThree, 
+  char.acrobatics, char.animalHandling, char.arcana, char.athletics, char.deception, char.history, char.insight, char.intimidation,
   char.investigation, char.medicine, char.nature, char.perception, char.performance, char.persuasion, char.religion, char.sleightOfHand,
-  char.stealth, char.survival, char.spellNotes, char.id];
+  char.stealth, char.survival,
+  char.acrobaticsProf, char.animalHandlingProf, char.arcanaProf, char.athleticsProf, char.deceptionProf, char.historyProf, char.insightProf, char.intimidationProf,
+  char.investigationProf, char.medicineProf, char.natureProf, char.perceptionProf, char.performanceProf, char.persuasionProf, char.religionProf, char.sleightOfHandProf,
+  char.stealthProf, char.survivalProf, char.spellNotes, char.id];
   let sql = `UPDATE 'main'.'tab_characters'
               SET char_name = ?, char_player = ?, char_prof = ?, char_exp = ?, char_pic = ?, char_classes = ?, char_race = ?, char_background = ?, 
               char_ac = ?, char_hp = ?, char_hp_current = ?, char_hitDice = ?, char_init = ?, char_speed = ?, char_str = ?, char_dex = ?, char_con = ?, char_int = ?, char_wis = ?, 
@@ -542,7 +545,10 @@ const saveChar = (char) => {
               char_notesTwo = ?, char_notesThree = ?, char_acrobatics = ?,   char_animalHandling = ?, 
               char_arcana = ?, char_athletics = ?, char_deception = ?, char_history = ?, char_insight = ?, char_intimidation = ?, char_investigation = ?, 
               char_medicine = ?, char_nature = ?, char_perception = ?, char_performance = ?, char_persuasion = ?, char_religion = ?, 
-              char_sleightOfHand = ?, char_stealth = ?, char_survival = ?, char_spellNotes = ?
+              char_sleightOfHand = ?, char_stealth = ?, char_survival = ?, char_acrobaticsProf = ?,   char_animalHandlingProf = ?, 
+              char_arcanaProf = ?, char_athleticsProf = ?, char_deceptionProf = ?, char_historyProf = ?, char_insightProf = ?, char_intimidationProf = ?, char_investigationProf = ?, 
+              char_medicineProf = ?, char_natureProf = ?, char_perceptionProf = ?, char_performanceProf = ?, char_persuasionProf = ?, char_religionProf = ?, 
+              char_sleightOfHandProf = ?, char_stealthProf = ?, char_survivalProf = ?, char_spellNotes = ?
               WHERE char_id = ?`;
   db.serialize(function () {
     db.run(sql, data, function (err) {
--- src/components/char/CharView.js
+++ src/components/char/CharView.js
@@ -58,23 +58,41 @@ export default function CharView(props) {
     const [notesThree, setNotesThree] = useState("");
 
     const [acrobatics, setAcrobatics] = useState(0);
+    const [acrobaticsProf, setAcrobaticsProf] = useState(0);
     const [animalHandling, setAnimalHandling] = useState(0);
+    const [animalHandlingProf, setAnimalHandlingProf] = useState(0);
     const [arcana, setArcana] = useState(0);
+    const [arcanaProf, setArcanaProf] = useState(0);
     const [athletics, setAthletics] = useState(0);
+    const [athleticsProf, setAthleticsProf] = useState(0);
     const [deception, setDeception] = useState(0);
+    const [deceptionProf, setDeceptionProf] = useState(0);
     const [history, setHistory] = useState(0);
+    const [historyProf, setHistoryProf] = useState(0);
     const [insight, setInsight] = useState(0);
+    const [insightProf, setInsightProf] = useState(0);
     const [intimidation, setIntimidation] = useState(0);
+    const [intimidationProf, setIntimidationProf] = useState(0);
     const [investigation, setInvestigation] = useState(0);
+    const [investigationProf, setInvestigationProf] = useState(0);
     const [medicine, setMedicine] = useState(0);
+    const [medicineProf, setMedicineProf] = useState(0);
     const [nature, setNature] = useState(0);
+    const [natureProf, setNatureProf] = useState(0);
     const [perception, setPerception] = useState(0);
+    const [perceptionProf, setPerceptionProf] = useState(0);
     const [performance, setPerformance] = useState(0);
+    const [performanceProf, setPerformanceProf] = useState(0);
     const [persuasion, setPersuasion] = useState(0);
+    const [persuasionProf, setPersuasionProf] = useState(0);
     const [religion, setReligion] = useState(0);
+    const [religionProf, setReligionProf] = useState(0);
     const [sleightOfHand, setSleightOfHand] = useState(0);
+    const [sleightOfHandProf, setSleightOfHandProf] = useState(0);
     const [stealth, setStealth] = useState(0);
+    const [stealthProf, setStealthProf] = useState(0);
     const [survival, setSurvival] = useState(0);
+    const [survivalProf, setSurvivalProf] = useState(0);
 
     const [spellNotes, setSpellNotes] = useState("");
     const [spells, setSpells] = useState([]);
@@ -129,23 +147,41 @@ export default function CharView(props) {
         setNotesThree(result.char_notesThree);
 
         setAcrobatics(result.char_acrobatics);
+        setAcrobaticsProf(result.char_acrobaticsProf);
         setAnimalHandling(result.char_animalHandling);
+        setAnimalHandlingProf(result.char_animalHandlingProf);
         setArcana(result.char_arcana);
+        setArcanaProf(result.char_arcanaProf);
         setAthletics(result.char_athletics);
+        setAthleticsProf(result.char_athleticsProf);
         setDeception(result.char_deception);
+        setDeceptionProf(result.char_deceptionProf);
         setHistory(result.char_history);
+        setHistoryProf(result.char_historyProf);
         setInsight(result.char_insight);
+        setInsightProf(result.char_insightProf);
         setIntimidation(result.char_intimidation);
+        setIntimidationProf(result.char_intimidationProf);
         setInvestigation(result.char_investigation);
+        setInvestigationProf(result.char_investigationProf);
         setMedicine(result.char_medicine);
+        setMedicineProf(result.char_medicineProf);
         setNature(result.char_nature);
+        setNatureProf(result.char_natureProf);
         setPerception(result.char_perception);
+        setPerceptionProf(result.char_perceptionProf);
         setPerformance(result.char_performance);
+        setPerformanceProf(result.char_performanceProf);
         setPersuasion(result.char_persuasion);
+        setPersuasionProf(result.char_persuasionProf);
         setReligion(result.char_religion);
+        setReligionProf(result.char_religionProf);
         setSleightOfHand(result.char_sleightOfHand);
+        setSleightOfHandProf(result.char_sleightOfHandProf);
         setStealth(result.char_stealth);
+        setStealthProf(result.char_stealthProf);
         setSurvival(result.char_survival);
+        setSurvivalProf(result.char_survivalProf);
 
         setSpellNotes(result.char_spellNotes);
     }
@@ -215,7 +251,10 @@ export default function CharView(props) {
                 senses, passivPerception, passivInsight, passivInvestigation,
                 notesOne, notesTwo, notesThree, acrobatics, animalHandling, arcana,
                 athletics, deception, history, insight, intimidation, investigation, medicine, nature,
-                perception, performance, persuasion, religion, sleightOfHand, stealth, survival, spellNotes
+                perception, performance, persuasion, religion, sleightOfHand, stealth, survival,
+                acrobaticsProf, animalHandlingProf, arcanaProf,
+                athleticsProf, deceptionProf, historyProf, insightProf, intimidationProf, investigationProf, medicineProf, natureProf,
+                perceptionProf, performanceProf, persuasionProf, religionProf, sleightOfHandProf, stealthProf, survivalProf, spellNotes
             }
         });
         ipcRenderer.send('saveCharItems', {
@@ -271,6 +310,16 @@ export default function CharView(props) {
         })
         setSpells(allItemsWithChangedSingleSpell);
     }, [spells, setSpells]);
+
+    const changeIcon = (value) => {
+        if (value === undefined || value === 0) {
+            return faMinus;
+        } else if (value === 1) {
+            return faAngleUp;
+        } else {
+            return faAngleDoubleUp;
+        }
+    }
 
     const style = {
         backgroundImage: `url(${pic})`,
@@ -386,32 +435,50 @@ export default function CharView(props) {
                             </div>
                             <div className="skills">
                                 <div classame="skillRow">
-                                    <label>Acrobatics (Dex): <input type="number" value={acrobatics} onChange={e => setAcrobatics(e.target.value)}></input><input type="radio" /></label>
-                                    <label>Animal Handling (Wis): <input type="number" value={animalHandling} onChange={e => setAnimalHandling(e.target.value)}></input><input type="radio" /></label>
-                                    <label>Arcana (Int): <input type="number" value={arcana} onChange={e => setArcana(e.target.value)}></input><input type="radio" /></label>
-                                    <label>Athletics (Str): <input type="number" value={athletics} onChange={e => setAthletics(e.target.value)}></input><input type="radio" /></label>
-                                    <label>Deception (Cha): <input type="number" value={deception} onChange={e => setDeception(e.target.value)}></input><input type="radio" /></label>
-                                    <label>History (Int): <input type="number" value={history} onChange={e => setHistory(e.target.value)}></input><input type="radio" /></label>
+                                    <label>Acrobatics (Dex): <input type="number" value={acrobatics} onChange={e => setAcrobatics(e.target.value)} />
+                                        <FontAwesomeIcon className="profToggler" icon={changeIcon(acrobaticsProf)} onClick={e => setAcrobaticsProf((acrobaticsProf + 1) % 3)} /></label>
+                                    <label>Animal Handling (Wis): <input type="number" value={animalHandling} onChange={e => setAnimalHandling(e.target.value)} />
+                                        <FontAwesomeIcon className="profToggler" icon={changeIcon(animalHandlingProf)} onClick={e => setAnimalHandlingProf((animalHandlingProf + 1) % 3)} /></label>
+                                    <label>Arcana (Int): <input type="number" value={arcana} onChange={e => setArcana(e.target.value)} />
+                                        <FontAwesomeIcon className="profToggler" icon={changeIcon(arcanaProf)} onClick={e => setArcanaProf((arcanaProf + 1) % 3)} /></label>
+                                    <label>Athletics (Str): <input type="number" value={athletics} onChange={e => setAthletics(e.target.value)} />
+                                        <FontAwesomeIcon className="profToggler" icon={changeIcon(athleticsProf)} onClick={e => setAthleticsProf((athleticsProf + 1) % 3)} /></label>
+                                    <label>Deception (Cha): <input type="number" value={deception} onChange={e => setDeception(e.target.value)} />
+                                        <FontAwesomeIcon className="profToggler" icon={changeIcon(deceptionProf)} onClick={e => setDeceptionProf((deceptionProf + 1) % 3)} /></label>
+                                    <label>History (Int): <input type="number" value={history} onChange={e => setHistory(e.target.value)} />
+                                        <FontAwesomeIcon className="profToggler" icon={changeIcon(historyProf)} onClick={e => setHistoryProf((historyProf + 1) % 3)} /></label>
                                 </div>
                             </div>
                             <div className="skills">
                                 <div classame="skillRow">
-                                    <label>Insight (Wis): <input type="number" value={insight} onChange={e => setInsight(e.target.value)}></input><input type="radio" /></label>
-                                    <label>Intimidation (Cha): <input type="number" value={intimidation} onChange={e => setIntimidation(e.target.value)}></input><input type="radio" /></label>
-                                    <label>Investigation (Int): <input type="number" value={investigation} onChange={e => setInvestigation(e.target.value)}></input><input type="radio" /></label>
-                                    <label>Medicine (Wis): <input type="number" value={medicine} onChange={e => setMedicine(e.target.value)}></input><input type="radio" /></label>
-                                    <label>Nature (Int): <input type="number" value={nature} onChange={e => setNature(e.target.value)}></input><input type="radio" /></label>
-                                    <label>Perception (Wis): <input type="number" value={perception} onChange={e => setPerception(e.target.value)}></input><input type="radio" /></label>
+                                    <label>Insight (Wis): <input type="number" value={insight} onChange={e => setInsight(e.target.value)} />
+                                        <FontAwesomeIcon className="profToggler" icon={changeIcon(insightProf)} onClick={e => setInsightProf((insightProf + 1) % 3)} /></label>
+                                    <label>Intimidation (Cha): <input type="number" value={intimidation} onChange={e => setIntimidation(e.target.value)} />
+                                        <FontAwesomeIcon className="profToggler" icon={changeIcon(intimidationProf)} onClick={e => setIntimidationProf((intimidationProf + 1) % 3)} /></label>
+                                    <label>Investigation (Int): <input type="number" value={investigation} onChange={e => setInvestigation(e.target.value)} />
+                                        <FontAwesomeIcon className="profToggler" icon={changeIcon(investigationProf)} onClick={e => setInvestigationProf((investigationProf + 1) % 3)} /></label>
+                                    <label>Medicine (Wis): <input type="number" value={medicine} onChange={e => setMedicine(e.target.value)} />
+                                        <FontAwesomeIcon className="profToggler" icon={changeIcon(medicineProf)} onClick={e => setMedicineProf((medicineProf + 1) % 3)} /></label>
+                                    <label>Nature (Int): <input type="number" value={nature} onChange={e => setNature(e.target.value)} />
+                                        <FontAwesomeIcon className="profToggler" icon={changeIcon(natureProf)} onClick={e => setNatureProf((natureProf + 1) % 3)} /></label>
+                                    <label>Perception (Wis): <input type="number" value={perception} onChange={e => setPerception(e.target.value)} />
+                                        <FontAwesomeIcon className="profToggler" icon={changeIcon(perceptionProf)} onClick={e => setPerceptionProf((perceptionProf + 1) % 3)} /></label>
                                 </div>
                             </div>
                             <div className="skills">
                                 <div classame="skillRow">
-                                    <label>Performance (Cha): <input type="number" value={performance} onChange={e => setPerformance(e.target.value)}></input><input type="radio" /></label>
-                                    <label>Persuasion (Cha): <input type="number" value={persuasion} onChange={e => setPersuasion(e.target.value)}></input><input type="radio" /></label>
-                                    <label>Religion (Int): <input type="number" value={religion} onChange={e => setReligion(e.target.value)}></input><input type="radio" /></label>
-                                    <label>Sleight of Hand (Dex): <input type="number" value={sleightOfHand} onChange={e => setSleightOfHand(e.target.value)}></input><input type="radio" /></label>
-                                    <label>Stealth (Dex): <input type="number" value={stealth} onChange={e => setStealth(e.target.value)}></input><input type="radio" /></label>
-                                    <label>Survival (Wis): <input type="number" value={survival} onChange={e => setSurvival(e.target.value)}></input><input type="radio" /></label>
+                                    <label>Performance (Cha): <input type="number" value={performance} onChange={e => setPerformance(e.target.value)} />
+                                        <FontAwesomeIcon className="profToggler" icon={changeIcon(performanceProf)} onClick={e => setPerformanceProf((performanceProf + 1) % 3)} /></label>
+                                    <label>Persuasion (Cha): <input type="number" value={persuasion} onChange={e => setPersuasion(e.target.value)} />
+                                        <FontAwesomeIcon className="profToggler" icon={changeIcon(persuasionProf)} onClick={e => setPersuasionProf((persuasionProf + 1) % 3)} /></label>
+                                    <label>Religion (Int): <input type="number" value={religion} onChange={e => setReligion(e.target.value)} />
+                                        <FontAwesomeIcon className="profToggler" icon={changeIcon(religionProf)} onClick={e => setReligionProf((religionProf + 1) % 3)} /></label>
+                                    <label>Sleight of Hand (Dex): <input type="number" value={sleightOfHand} onChange={e => setSleightOfHand(e.target.value)} />
+                                        <FontAwesomeIcon className="profToggler" icon={changeIcon(sleightOfHandProf)} onClick={e => setSleightOfHandProf((sleightOfHandProf + 1) % 3)} /></label>
+                                    <label>Stealth (Dex): <input type="number" value={stealth} onChange={e => setStealth(e.target.value)} />
+                                        <FontAwesomeIcon className="profToggler" icon={changeIcon(stealthProf)} onClick={e => setStealthProf((stealthProf + 1) % 3)} /></label>
+                                    <label>Survival (Wis): <input type="number" value={survival} onChange={e => setSurvival(e.target.value)} />
+                                        <FontAwesomeIcon className="profToggler" icon={changeIcon(survivalProf)} onClick={e => setSurvivalProf((survivalProf + 1) % 3)} /></label>
                                 </div>
                             </div>
                             <div className="smallLabelGroup">
