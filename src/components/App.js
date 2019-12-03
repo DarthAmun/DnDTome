688cababe3ce3e41f8acccf078b1932df6ee7e08
DarthAmun <37881139+DarthAmun@users.noreply.github.com>
2019-12-03 16:43:51 +0100

    init rework app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,109 +1,71 @@
 import '../assets/css/App.css';
-import React, { useState, useEffect } from 'react';
-import { MemoryRouter, Switch, Route } from 'react-router';
+import React, { Component, useEffect } from 'react';
+import { MemoryRouter, Switch, Route, useLocation } from 'react-router';
 
 import SpellOverview from './spell/SpellOverview';
-import CharOverview from './char/CharOverview';
 import ItemOverview from './item/ItemOverview';
-import GearOverview from './gear/GearOverview';
 import MonsterOverview from './monster/MonsterOverview';
-
-import CharView from './char/CharView';
 
 import Home from './Home';
 import Options from './Options';
 
 import AddSpell from './add/AddSpell';
 import AddItem from './add/AddItem';
-import AddGear from './add/AddGear';
 import AddMonster from './add/AddMonster';
-import AddChar from './add/AddChar';
+
+import CharView from './char/CharView';
 
 import LeftNav from './LeftNav';
 import TopNav from './TopNav';
-import Notification from './Notification';
 
-import packageJson from '../../package.json';
-import OptionService from '../database/OptionService';
-import ThemeService from '../services/ThemeService';
-
-export function PageLayout(props) {
-
-  const catchDrop = (e) => {
-    e.preventDefault();
-  }
-
-  return (
-    <div className="App" onDrop={e => catchDrop}>
-      <Notification />
-      <LeftNav />
-      <div id="content">
-        <TopNav />
-        {props.children}
-      </div>
-      <div id="credits">v{packageJson.version} by DarthAmun</div>
+const PageLayout = ({ children }) => (
+  <div className="App">
+    <LeftNav />
+    <div id="content">
+      <TopNav />
+      {children}
     </div>
-  );
+    <div id="credits">by DarthAmun</div>
+  </div>
+);
 
-}
+const HomeLayout = ({ children }) => (
+  <div className="App homeDrag">
+    <div id="content">{children}</div>
+    <div id="credits">by DarthAmun</div>
+  </div>
+);
 
-export default function App() {
+const LayoutRoute = ({ component: Component, layout: Layout, ...rest }) => (
+  <Route
+    {...rest}
+    render={props => (
+      <Layout>
+        <Component {...props} />
+      </Layout>
+    )}
+  />
+);
 
-  useEffect(() => {
-    OptionService.get('theme', function (result) {
-      ThemeService.setTheme(result);
-      ThemeService.applyTheme(result);
-    });
-  }, []);
-
+const App = () => {
+  const theme = useContext(themeContext)
   return (
-    <MemoryRouter>
-      <Switch>
-        <Route path="/spell-overview" render={() => {
-          return <PageLayout><SpellOverview /></PageLayout>
-        }} />
-        <Route path="/item-overview" render={() => {
-          return <PageLayout><ItemOverview /></PageLayout>
-        }} />
-        <Route path="/gear-overview" render={() => {
-          return <PageLayout><GearOverview /></PageLayout>
-        }} />
-        <Route path="/monster-overview" render={() => {
-          return <PageLayout><MonsterOverview /></PageLayout>
-        }} />
-        <Route path="/char-overview" render={() => {
-          return <PageLayout><CharOverview /></PageLayout>
-        }} />
-        <Route path="/char/:id" render={(props) => {
-          return <PageLayout><CharView {...props} /></PageLayout>
-        }} />
-        <Route path="/add-spell" render={() => {
-          return <PageLayout><AddSpell /></PageLayout>
-        }} />
-        <Route path="/add-item" render={() => {
-          return <PageLayout><AddItem /></PageLayout>
-        }} />
-        <Route path="/add-gear" render={() => {
-          return <PageLayout><AddGear /></PageLayout>
-        }} />
-        <Route path="/add-monster" render={() => {
-          return <PageLayout><AddMonster /></PageLayout>
-        }} />
-        <Route path="/add-char" render={() => {
-          return <PageLayout><AddChar /></PageLayout>
-        }} />
-        <Route path="/options" render={() => {
-          return <PageLayout><Options /></PageLayout>
-        }} />
-        <Route path="/" render={() => {
-          return <div className="App homeDrag">
-            <div id="content">
-              <Home/>
-            </div>
-            <div id="credits">v{packageJson.version} by DarthAmun</div>
-          </div>
-        }} />
-      </Switch>
-    </MemoryRouter>
+    <ThemeContext.Provider value={theme}>
+      <MemoryRouter>
+        <Switch>
+          <LayoutRoute path="/spell-overview" layout={PageLayout} component={SpellOverview} />
+          <LayoutRoute path="/item-overview" layout={PageLayout} component={ItemOverview} />
+          <LayoutRoute path="/monster-overview" layout={PageLayout} component={MonsterOverview} />
+          <LayoutRoute path="/char/:id" layout={PageLayout} component={CharView} />
+          <LayoutRoute path="/add-spell" layout={PageLayout} component={AddSpell} />
+          <LayoutRoute path="/add-item" layout={PageLayout} component={AddItem} />
+          <LayoutRoute path="/add-monster" layout={PageLayout} component={AddMonster} />
+          <LayoutRoute path="/options" layout={PageLayout} component={Options} />
+          <LayoutRoute path="/" layout={HomeLayout} component={Home} />
+        </Switch>
+      </MemoryRouter >
+    </ThemeContext.Provider>
   );
-}
+};
+
+export default App;
